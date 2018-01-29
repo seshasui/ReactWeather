f08a3c619b21801cd819f34cf2882b7319af90e1
seshasui <seshaui@gmail.com>
2018-01-29 16:00:43 -0500

    change to Examples

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,7 +1,9 @@
 var React = require('react');
 var Examples = (props) => {
   return (
-    <h3>Examples Component</h3>
+    <div>
+      <h3>Examples</h3>
+    </div>
   );
 }
 module.exports = Examples;
