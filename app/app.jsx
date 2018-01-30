a6e0146520fea27eba25376e5135b814150303d6
seshasui <seshaui@gmail.com>
2018-01-29 21:38:33 -0500

    introduce foundation

--- app/app.jsx
+++ app/app.jsx
@@ -5,7 +5,10 @@ var Main = require('Main');
 var Weather = require('Weather');
 var About = require('About');
 var Examples = require('Examples');
-
+//load foundation
+require('style!css!foundation-sites/dist/foundation.min.css');
+debugger;
+$(document).foundation();
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={Main}>
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 // Create our app
 var app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4200;
 app.use(function(req, res, next){
   if(req.headers['x-forwarded-proto'] === 'https'){
     res.redirect('http://' + req.hostname + req.url);
